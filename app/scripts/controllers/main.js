cfd340f2fe48e3eb6314e7476cce9685d22a7393
harjones <harjones@cisco.com>
2015-02-26 10:41:18 -0800

    Directory Structure Added
    Yeoman Angular-Generator directory structure, plus minor bug fixes in
    the generator searching the wrong directory for grunt

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bmpUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bmpUiApp
+ */
+angular.module('bmpUiApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
