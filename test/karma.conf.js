e420b306c417199d0fbb366f4131dc7a9a829275
harjones <harjones@cisco.com>
2015-03-02 14:03:14 -0800

    Fixed karma grunt:test issues
    default karma was evaluating test cases on nonpresent controller code,
    updated and fixed.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -26,8 +26,8 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
-      'app/scripts/**/*.js',
-      'test/mock/**/*.js',
+      'app/views/**/*.js',
+      //'test/mock/**/*.js',
       'test/spec/**/*.js'
     ],
 
