432e331a6bf203590f7780badc85a612fc50b533
Alex Austin <aleausti@cisco.com>
2015-03-02 16:24:11 -0800

    Added Bmp Server table to Dash (using mostly rom code) added api call to controller

--- app/views/dashboard/dashboard.js
+++ app/views/dashboard/dashboard.js
@@ -9,6 +9,15 @@
  */
 angular.module('bmpUiApp')
   .controller('DashboardController', function ($scope) {
+    // Active routers ( draw on BMP Server tab )
+    jQuery.ajax({
+      url: "http://odl-dev.openbmp.org:8001/db_rest/v1/routers/status/up",
+      success: function (result) {
+        $scope.active_routers = result.routers.size;
+      },
+      async: false
+    });
+
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
