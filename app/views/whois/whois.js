2bbe4145ad438408a3dff1a7dd021dc7e818fd31
Alex Austin <aleausti@cisco.com>
2015-03-06 13:50:48 -0800

    whois page change

--- app/views/whois/whois.js
+++ app/views/whois/whois.js
@@ -10,4 +10,29 @@
 angular.module('bmpUiApp')
   .controller('WhoIsController', function ($scope) {
 
+    function api_call(apiurl) {
+      var res;
+
+      jQuery.ajax({
+        url: apiurl,
+        success: function (result) {
+          res = result;
+        },
+        async: false
+      });
+      return res;
+    }
+
+    //Loop through data selecting and altering relevant data.
+    $scope.whoIsGrid = [];
+    for(var i = 0; i < $scope.routers.length; i++) {
+      var item = {};
+      //alter item
+      //HERE
+
+      //add item
+      $scope.whoIsGrid.push(item);
+    }
+    $scope.whoIsGridOptions = { data: 'whoIsGrid' };
+
   });
