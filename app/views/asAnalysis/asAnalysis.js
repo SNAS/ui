c57c3a13cec102b5556d5d594e31dee409681fb0
Jiarou Fan <jiarfan@cisco.com>
2015-03-09 19:08:43 -0700

    Added threes charts for AS analysis
    Added threes charts for AS analysis

--- app/views/asAnalysis/asAnalysis.js
+++ app/views/asAnalysis/asAnalysis.js
@@ -8,7 +8,9 @@
  * Controller of the login page
  */
 angular.module('bmpUiApp')
-  .controller('ASAnalysisController', function ($scope, $http) {
+  .controller('ASAnalysisController',['$scope', '$http','apiFactory', function ($scope, $http, apiFactory) {
+
+ //   $scope.transit = true;
 
     /* Chart options */
     $scope.options = {
@@ -39,39 +41,89 @@ angular.module('bmpUiApp')
     };
 
     /* Chart data */
-    $scope.transit_ipv4_data = [];
+    $scope.top_transit_ipv4_data = [];
+    $scope.top_transit_ipv6_data = [];
+    $scope.top_origin_ipv4_data = [];
+    $scope.top_origin_ipv6_data = [];
 
     getData();
 
+    /* Get the data of charts */
     function getData(){
-      $http.get('http://demo.openbmp.org:8001/db_rest/v1/as_stats/ipv4?topTransit=5').success(
-        function (results){
-          var ipv4 = results.s.data;
-          var values=[];
-
-          //create array with amount of prefixes
-          for (var i = ipv4.length-1; i >=0; i--) {
-            var st = "";
-            if (typeof ipv4[i].as_name != 'undefined') {
-              st += ipv4[i].as_name;
-            }
-            st += ' (AS' + ipv4[i].asn + ')';
-            values.push({
-              "label":st,
-              "value":ipv4[i].transit_v4_prefixes
-            });
-          }
-          console.log(values);
-
-         $scope.transit_ipv4_data=[{
-            key: "Cumulative Return",
-            values: values
-          }];
+      apiFactory.getTopTransitIpv4Data().success(
+        function(results){
+          $scope.top_transit_ipv4_data = processData(results,"transit_v4_prefixes");
         }
       ).
         error(function (error){
           console.log(error.message);
-        })
+        });
+
+      apiFactory.getTopTransitIpv6Data().success(
+        function(results){
+          $scope.top_transit_ipv6_data = processData(results,"transit_v6_prefixes");
+        }
+      ).
+        error(function (error){
+          console.log(error.message);
+        });
+
+      apiFactory.getTopOriginIpv4Data().success(
+        function(results){
+          $scope.top_origin_ipv4_data = processData(results,"origin_v4_prefixes");
+        }
+      ).
+        error(function (error){
+          console.log(error.message);
+        });
+
+      apiFactory.getTopOriginIpv6Data().success(
+        function(results){
+          $scope.top_origin_ipv6_data = processData(results,"origin_v6_prefixes");
+        }
+      ).
+        error(function (error){
+          console.log(error.message);
+        });
+
     }
 
-  });
+    /* Process the data get from the APIs */
+    function processData(results,option){
+      var ip = results.s.data;
+      var values=[];
+      var prefixes;
+
+      //create array with amount of prefixes
+      for (var i = ip.length-1; i >=0; i--) {
+        if(option == "transit_v4_prefixes" ) {
+          prefixes = ip[i].transit_v4_prefixes;
+        }else if(option == "transit_v6_prefixes" ) {
+          prefixes = ip[i].transit_v6_prefixes;
+        }else if(option == "origin_v4_prefixes" ) {
+          prefixes = ip[i].origin_v4_prefixes;
+        }else if(option == "origin_v6_prefixes" ) {
+          prefixes = ip[i].origin_v6_prefixes;
+        }
+
+        var st = "";
+        if (typeof ip[i].as_name != 'undefined') {
+          st += ip[i].as_name;
+        }
+        st += ' (AS' + ip[i].asn + ')';
+        values.push({
+          "label":st,
+          "value":prefixes
+        });
+      }
+      console.log(values);
+
+      var chart_data=[{
+        key: "Cumulative Return",
+        values: values
+      }];
+
+      return chart_data;
+    }
+
+  }]);
