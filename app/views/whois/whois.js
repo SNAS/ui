d3c0307142f7bc261b69f04ead464ca13234e5dc
Alex Austin <aleausti@cisco.com>
2015-03-18 13:50:39 -0700

    Changed WHOIS should all be done now

--- app/views/whois/whois.js
+++ app/views/whois/whois.js
@@ -27,23 +27,23 @@ angular.module('bmpUiApp')
 
     $scope.whoIsGridOptions.columnDefs = [
       { name: "asn", displayName: 'ASN' },
-      { name: "isTransit", displayName: 'Transit' },
-      { name: "isOrigin", displayName: 'Origin' },
       { name: "as_name", displayName: 'AS Name' },
       { name: "org_name", displayName: 'ORG Name' },
-      { name: "country", displayName: 'Country' }
+      { name: "country", displayName: 'Country' },
+      { name: "symbTransit", displayName: 'Transit' },
+      { name: "symbOrigin", displayName: 'Origin' }
     ];
 
     $scope.whoIsGridOptions.multiSelect = false;
-    $scope.whoIsGridOptions.modifierKeysToMultiSelect = false;
     $scope.whoIsGridOptions.noUnselect = true;
+    $scope.whoIsGridOptions.modifierKeysToMultiSelect = false;
     $scope.whoIsGridOptions.rowTemplate = '<div ng-click="grid.appScope.changeSelected();" ng-repeat="col in colContainer.renderedColumns track by col.colDef.name" class="ui-grid-cell" ui-grid-cell></div>';
     $scope.whoIsGridOptions.onRegisterApi = function (gridApi) {
       $scope.whoIsGridApi = gridApi;
     };
 
     //Loop through data selecting and altering relevant data.
-    var searchValue = function(value) {
+    var searchValue = function(value,init) {
       if(value == "" || value == " ")
         return;
       var numberRegex = /^\d+$/;
@@ -52,8 +52,11 @@ angular.module('bmpUiApp')
         //  not a number do string search
         apiFactory.getWhoIsName(value).
           success(function (result){
-            $scope.whoIsData = result.w.data;
+            $scope.whoIsGridOptions.data = $scope.whoIsData = result.w.data;
             createWhoIsDataGrid();
+            if(init){
+              initSelect();
+            }
           }).
           error(function (error){
             console.log(error.message);
@@ -62,7 +65,7 @@ angular.module('bmpUiApp')
         // do a asn search
         apiFactory.getWhoIsWhereASN(value).
           success(function (result){
-            $scope.whoIsData = result.w.data;
+            $scope.whoIsGridOptions.data = $scope.whoIsData = result.w.data;
             createWhoIsDataGrid();
           }).
           error(function (error){
@@ -70,8 +73,18 @@ angular.module('bmpUiApp')
           });
       }
     };
-    //Init
-    searchValue("Cisco");
+
+    var initSelect = function(){
+      $timeout(function () {
+        if ($scope.whoIsGridApi.selection.selectRow) {
+          $scope.whoIsGridApi.selection.selectRow($scope.whoIsGridOptions.data[0]);
+        }
+      });
+      $timeout(function () {
+        $scope.changeSelected();
+      });
+    };
+
     //$interval( function() {$scope.gridApi.selection.selectRow($scope.gridOptions.data[0]);}, 0, 1);
 
     //Waits a bit for user to contiune typing.
@@ -85,47 +98,69 @@ angular.module('bmpUiApp')
       }, 500);
     };
 
-    //Not used
     var createWhoIsDataGrid = function () {
-      $scope.whoIsGrid = [];
       for(var i = 0; i <  $scope.whoIsData.length; i++) {
-        if($scope.whoIsData[i].isTransit == 1){
-          $scope.symbTransit = "✔";
-        }else{
-          $scope.symbTransit = "✘";
-        }
-        if($scope.whoIsData[i].isOrigin == 1){
-          $scope.symbOrigin = "✔";
-        }else{
-          $scope.symbOrigin = "✘";
-        }
-        $scope.whoIsGrid.push({
-            "asn":  $scope.whoIsData[i].asn,
-            "isTransit": $scope.symbTransit,
-            "isOrigin": $scope.symbOrigin,
-            "as_name": $scope.whoIsData[i].as_name,
-            "org_name": $scope.whoIsData[i].org_name,
-            "country": $scope.whoIsData[i].country
-        });
+        $scope.whoIsData[i].symbTransit = ($scope.whoIsData[i].isTransit == 1)? "✔":"✘";
+        $scope.whoIsData[i].symbOrigin = ($scope.whoIsData[i].isOrigin == 1)? "✔":"✘";
       }
-      $scope.whoIsGridOptions.data = $scope.whoIsGrid;
-      //$interval( function() {$scope.whoIsGridApi.selection.selectRow($scope.whoIsGridOptions.data[0]);}, 0, 1);
     };
 
     var resize = function() {
       $scope.whoIsGridApi.core.handleWindowResize();
     };
 
+    //Decided to put the data into a table in the pre to align it
     $scope.changeSelected = function() {
+      var noShow = ["$$hashKey","symbOrigin","symbTransit"];
       var values = $scope.whoIsGridApi.selection.getSelectedRows()[0];
-      console.dir(values);
-      var showValues = "";
+      var showValues = '<table>';
 
       angular.forEach(values, function(value, key) {
-        console.dir(value + ": " + key);
-        showValues+= "" + value + ": " + key;
+        if(noShow.indexOf(key)== -1){ //doesnt show certain fields
+          if(key == "raw_output"){
+            //Process each raw output field
+            var raw_data = value.split("\n");
+            showValues+='<tr><td> </td></tr>'; //br from top
+
+            for(var i = 0; i < raw_data.length; i++){
+              showValues+='<tr>';
+              if(raw_data[i]=="") { //was a newline
+                showValues+= '<td> </td>';
+              }else{
+                var s = raw_data[i].split(": "); //split on :<space> cause of http:// links
+
+                showValues+=(
+                  '<td>'+
+                    s[0].trim()+': '+
+                  '</td>'+
+
+                  '<td>'+
+                    s[1].trim()+
+                  '</td>'
+                );
+              }
+              showValues+='</tr>';
+            }
+          }else{
+            showValues+= (
+              '<tr>'+
+                '<td>'+
+                   key + ': '+
+                '</td>'+
+
+                '<td>'+
+                  value+
+                '</td>'+
+              '</tr>'
+            );
+          }
+        }
       });
+      showValues+='</table>';
+
       $scope.selectedItem = showValues;
-      console.dir(showValues);
     };
+
+    //Init table data
+    searchValue("Cisco",true);
   }]);
