2009ac2f8c14c1b4dbf0b425b4a78ec15f50bb96
harjones <harjones@cisco.com>
2015-04-14 17:31:57 -0700

    popup hover, active clusters, updated icons

--- app/views/globalView/globalView.js
+++ app/views/globalView/globalView.js
@@ -9,5 +9,14 @@
  */
 angular.module('bmpUiApp')
 .controller('GlobalViewController', function ($scope, $state, $q, $http, $timeout, apiFactory, leafletData) {
-    window.SCOPE = $scope;
+    $scope.router;
+    $scope.peer;
+
+    $scope.$watch('router', function (val){
+        //console.log(val);
+    });
+
+    $scope.$watch('peer', function (val){
+        //console.log(val);
+    });
 });
