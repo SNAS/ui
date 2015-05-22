8e11caec252602c6028f4427e5ffebaaac846936
Harry Jones <harjones@cisco.com>
2015-05-21 17:14:57 -0700

    [BETA] rewrote globalView for dual window

---
+++ app/views/globalView/globalView.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bmpUiApp.controller:DashboardController
+ * @description
+ * # DashboardController
+ * Controller of the Dashboard page
+ */
+angular.module('bmpUiApp')
+.controller('GlobalViewController', function ($scope, $rootScope) {
+    window.VIEWSCOPE = $scope;
+
+    if($rootScope.dualWindow.active){
+        if($rootScope.dualWindow.a === "globalView" && !$rootScope.dualWindow['map-top']){
+            $rootScope.dualWindow['map-top'] = true;
+            $scope.name = 'dual-global-view-map-a';
+        }
+        else if($rootScope.dualWindow.b === "globalView" && !$rootScope.dualWindow['map-bottom']){
+            $rootScope.dualWindow['map-bottom'] = true;
+            $scope.name = 'dual-global-view-map-b';
+        }
+        else{
+            console.log($rootScope.dualWindow);
+        }
+    }
+    else{
+    	$scope.name = 'global-view-map';
+    }
+
+    $scope.location = "globalView";
+});
