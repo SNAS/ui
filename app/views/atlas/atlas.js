8c8f0c6166a2d59a3d601f337c7cb11ce5841e78
tingrli <tingrli@cisco.com>
2016-02-17 16:42:21 -0800

    Atlas View: removed unneeded code

--- app/views/atlas/atlas.js
+++ app/views/atlas/atlas.js
@@ -245,15 +245,18 @@ angular.module('bmpUiApp')
             var geoData = geo.v_geo_ip.data[0];
             var lat = geoData.latitude;
             var long = geoData.longitude;
-            var circle = L.circle([lat, long], 300, {
-                color: "#3F6C45"
-                //fillColor: 'white',
-                //fillOpacity: 0.5
-              })
-              .addTo(map).bindPopup("<p>" + "Prefix: " + fullPrefix + "</p>"
+            var circle = L.circleMarker([lat, long], {
+              color: "green",
+              fillColor: "#CCCCCC"
+            }).on('mouseover', function (e) {
+              e.target.openPopup();
+            }).on('mouseout', function (e) {
+              e.target.closePopup();
+            }).bindPopup("<p>" + "Prefix: " + fullPrefix + "</p>"
                 + "<p>" + "Peer Name: " + row.PeerName + "</p>"
                 + "<p>" + "Router Name: " + row.RouterName + "</p>"
-              );
+              )
+              .addTo(map);
             circle.prefix = row;
             circle.fullPrefix = fullPrefix;
             prefixCircles[fullPrefix] = circle;
@@ -296,22 +299,6 @@ angular.module('bmpUiApp')
     var ASCollection;
 
     var cluster;
-
-    var progress = document.getElementById('progress');
-    var progressBar = document.getElementById('progress-bar');
-
-    function updateProgressBar(processed, total, elapsed, layersArray) {
-      if (elapsed > 1000) {
-        // if it takes more than a second to load, display the progress bar:
-        progress.style.display = 'block';
-        progressBar.style.width = Math.round(processed / total * 100) + '%';
-      }
-
-      if (processed === total) {
-        // all markers processed - hide the progress bar:
-        progress.style.display = 'none';
-      }
-    }
 
     $scope.loading = true;
 
